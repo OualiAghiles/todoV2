e8596d094e59a704940f76e979738a71a86808aa
ouali aghiles <ouali.aghiles@gmail.com>
2019-02-05 21:39:48 +0100

    wip

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -21,7 +21,6 @@ var TodoController = (function () {
 
 
       if (data.lists.length > 0) {
-        debugger
         id = data.lists[data.lists.length - 1].id + 1
       } else {
         id = 0
@@ -37,7 +36,6 @@ var TodoController = (function () {
       var stat, newTodo;
       stat = false;
       newTodo = new Todos(stat, value, tabId);
-      console.log(data)
       data.lists[tabId].items.push(newTodo);
 
       return newTodo;
@@ -45,10 +43,10 @@ var TodoController = (function () {
     completeTodo: function (obj) {
       obj.status = !obj.status
     },
-    updateTodo: function(obj, newVal) {
+    updateTodo: function (obj, newVal) {
       obj.val = newVal
     },
-    removeTodo: function(obj) {
+    removeTodo: function (obj) {
       data.lists[obj.tabId].items.splice(data.lists[obj.tabId].items.indexOf(obj), 1)
     },
     testing: function () {
@@ -73,9 +71,9 @@ var UIController = (function () {
   // Public Object / Modules
   return {
     getInputValue: function (id) {
-        return {
-          titleInput: document.querySelector(DOMStrings.inputTitle).value
-        }
+      return {
+        titleInput: document.querySelector(DOMStrings.inputTitle).value
+      }
     },
     getInputTodo: function (id) {
       return {
@@ -85,13 +83,14 @@ var UIController = (function () {
     addListItem: function (obj) {
       var html, elem, tab;
       elem = document.querySelector(DOMStrings.tabs);
-      html = function() {
+      html = function () {
         return `<li class="tab col" id="${obj.id}">
           <a href="#${obj.val.titleInput}">${obj.val.titleInput}</a>
         </li>`;
       };
       elem.insertAdjacentHTML('beforeend', html());
-      tab = `<ul class="collection with-header active" id="${obj.val.titleInput}-${obj.id}">
+      tab =
+        `<ul class="collection with-header active" id="${obj.val.titleInput}-${obj.id}">
               <li class="collection-header row">
                 <h4 class=" col s5" >
                   <span class="">${obj.val.titleInput}</span>
@@ -103,15 +102,16 @@ var UIController = (function () {
                 <span class="col s1">
                   <button class="btnAddTodo" data-index="${obj.id}" data-parent="${obj.val.titleInput}-${obj.id}"><i class="material-icons">add</i></button>
                 </span>
-              
+
               </li>
             </ul>`;
       elem.insertAdjacentHTML('afterend', tab);
     },
-    addTodoItem: function(obj,tabId) {
+    addTodoItem: function (obj, tabId) {
       var todo, htmlTodo;
       todo = document.querySelector(`#${tabId}`);
-      htmlTodo = `<li class="collection-item">
+      htmlTodo =
+        `<li class="collection-item">
                     <div class="updateTodo row hide">
                       <input type="text" class="col s9">
                       <span class="col s3">
@@ -135,15 +135,15 @@ var UIController = (function () {
                   </li>`;
       todo.insertAdjacentHTML('beforeend', htmlTodo)
     },
-    completeTodo: function(todo) {
+    completeTodo: function (todo) {
       todo.classList.toggle('completed');
-      if(todo.querySelector('.done i').textContent === 'check') {
+      if (todo.querySelector('.done i').textContent === 'check') {
         todo.querySelector('.done i').textContent = 'crop_square';
       } else {
         todo.querySelector('.done i').textContent = 'check';
       }
     },
-    updatedTodo: function(obj, todo) {
+    updatedTodo: function (obj, todo) {
       var inputEdit = todo.querySelector('.updateTodo');
       inputEdit.classList.toggle("hide");
       inputEdit.querySelector('input').value = todo.querySelector('.content').textContent;
@@ -157,7 +157,7 @@ var UIController = (function () {
 
       })
     },
-    removedTodo: function(obj, todo) {
+    removedTodo: function (obj, todo) {
       todo.parentNode.removeChild(todo);
     },
     getDomStrings: function () {
@@ -167,36 +167,49 @@ var UIController = (function () {
 })();
 
 var LocalStor = (function () {
-  var addContent= function (data) {
+  var addTodosStor = function (arr) {
 
-    if(data !== null){
-      data.lists.forEach(function (item) {
-
-        UIController.addListItem(item)
-        var objs = Array.prototype.slice.call(item.items, 0)
-        objs.forEach(todo => {
-          UIController.addTodoItem(todo, `${item.val.titleInput}-${item.id}`)
-          AppController.addTodoController(`${item.val.titleInput}-${item.id}`)
-          AppController.todoActions(todo,item.val.titleInput)
-
-        })
-
-      })
+    for (var el in arr) {
+      UIController.addTodoItem(el, `${arr.val.titleInput}-${arr.id}`)
+      AppController.addTodoController(`${arr.val.titleInput}-${arr.id}`)
+      AppController.todoActions(el, arr.val.titleInput)
     }
-    return data
+  };
+  var addContent = function (data) {
+    if (data !== null) {
+      data.lists.forEach(function (item) {
+        UIController.addListItem(item)
+        addTodosStor(item)
+      })
+      /*for (var i = 0; i < data.lists.length; i++) {
+        item = data.lists[i]
+        UIController.addListItem(item)
+        console.log('item', item)
+        for (var elem = 0; elem < item.items.length; elem++) {
+
+          UIController.addTodoItem(elem, `${item.val.titleInput}-${item.id}`)
+          AppController.addTodoController(`${item.val.titleInput}-${item.id}`)
+          AppController.todoActions(elem, item.val.titleInput)
+
+        }
+      }*/
+
+    }
   };
   return {
-    addToStor: function (data) {
+    addToStor: function () {
       localStorage.setItem("data", JSON.stringify(data))
     },
     initStor: function () {
       //TodoController.data = localStorage.getItem("data")
+      data = JSON.parse(localStorage.getItem("data"))
 
-     var  data;
+
       //console.log(JSON.parse(localStorage.getItem("data")));
-      if (localStorage.getItem("data") !== null) {
+      if (data !== null) {
         data = JSON.parse(localStorage.getItem("data"))
         addContent(data)
+        return data
       } else {
         data = {
           lists: []
@@ -235,13 +248,12 @@ var AppController = (function (TodoCtrl, UICtrl, StoreData) {
     newList = TodoCtrl.addItem(value);
     // 3. Add the List in UI
     UICtrl.addListItem(newList);
-    if (DOM.tabTitleContentTodos !== null){
+    if (DOM.tabTitleContentTodos !== null) {
       var addTodoBtns = document.querySelector(DOM.tabTitleContentTodos);
       var addBtn = document.querySelector(DOM.btnAddTodo);
       addTodoBtns.addEventListener('click', function (e) {
-        if(e.target === addBtn) {
+        if (e.target === addBtn) {
           id = e.target.dataset.parent;
-          console.log(id)
           AppController.addTodoController(id);
         }
       });
@@ -256,7 +268,7 @@ var AppController = (function (TodoCtrl, UICtrl, StoreData) {
       newTodo = UICtrl.getInputTodo(id);
       tabId = id;
       tabId = tabId.split('-');
-      tabIndex = tabId[tabId.length -1];
+      tabIndex = tabId[tabId.length - 1];
       tabIndex = parseInt(tabIndex);
       val = newTodo.titleTab;
       //status, value, tabId
@@ -267,36 +279,37 @@ var AppController = (function (TodoCtrl, UICtrl, StoreData) {
       document.querySelector(DOM.inputTodo).value = '';
       AppController.todoActions(todoValue, todoValue.val);
     },
-    todoActions: function (obj ,contentToDo) {
+    todoActions: function (obj, contentToDo) {
       var todos = document.querySelectorAll(DOM.todos);
       todos.forEach(function (item) {
         if (item.querySelector('.content').textContent === contentToDo) {
           item.addEventListener('click', function (e) {
             var todo = e.target.parentNode.parentNode.parentNode.parentNode;
-            if(e.target.parentNode.classList.contains('done')){
+            if (e.target.parentNode.classList.contains('done')) {
               TodoCtrl.completeTodo(obj);
               UICtrl.completeTodo(todo);
               LocalStor.addToStor(data)
-            } else if(e.target.parentNode.classList.contains('edit')){
+            } else if (e.target.parentNode.classList.contains('edit')) {
               UICtrl.updatedTodo(obj, todo);
               LocalStor.addToStor(data)
-
-            } else if(e.target.parentNode.classList.contains('remove')){
+            } else if (e.target.parentNode.classList.contains('remove')) {
               UICtrl.removedTodo(obj, todo);
               TodoCtrl.removeTodo(obj);
-
               LocalStor.addToStor(data)
+
             }
+
           })
+
         }
       });
     },
     init: function () {
-      data =  StoreData.initStor()
+      var data;
+      data = StoreData.initStor()
       document.querySelector(DOM.inputTitle).value = '';
       setupEventListeners();
-      console.log(data);
-      return data
+      //return data
     }
   }
 })(TodoController, UIController, LocalStor);
